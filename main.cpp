9e75dbedf51d956148d7ae98edb06b9df17ab313
xebo <xebo@debian>
2024-05-04 10:33:43 -0400

    Instantiated Person class. Made attributes (?), constructor, getters and setters.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <string>
+#include "gtest/gtest.h"
+
+class Person{
+protected:
+  std::string name;
+  int age;
+  std::string address;
+public:
+  Person(std::string n, int a, std::string ad){ // constructor
+    name = n;
+    age = a;
+    address = ad;
+  }
+  // Getters and setters, something I just learned about.
+  // I assume that this would have S O M E administrative use (name changes, error correction)
+  // but boy is it going to be tedious
+  // but encapsulation yaaaaay
+
+  std::string getName(){return name;}
+  int getAge(){return age;}
+  std::string getAddress(){return address;}
+  
+  void setName(std::string n){name = n;}
+  void setAge(int a){age = a;}
+  void setAddress(std::string ad){address = ad;}
+
+};
 
 int main(){
   std::cout << "I am alive (not really)" << '\n'; // make this cooler, like, a bios boot screen
